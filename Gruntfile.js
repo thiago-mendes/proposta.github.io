e80ef53cd531411993058691f19e6747626b2994
thiago_almeida <thiago.mendes@convenia.com.br>
2016-03-24 14:55:02 -0300

    Estrutura básica

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+/*
+ *
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2014 Tyler Kellen, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		less: {
+			development: {
+				options: {
+					paths: ["css"]
+				},
+				files: {
+					"assets/css/main.css": ["assets/less/main.less"],
+				}
+			}
+		},
+		watch: {
+			files: ['assets/**/*.less','assets/less/*.less'],
+			tasks: ["less"]
+		}
+	});
+
+	grunt.registerTask('default', ['less','watch']);
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+}
